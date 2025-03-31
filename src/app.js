d524056df01126ca36a8a13bd9d85c5eb6f8c100
Dwane Dunn <dwane.dunn@gmail.com>
2025-03-31 17:57:59 -0400

    Create js function Generate Excuse

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import './style.css';
 
+import './assets/img/rigo-baby.jpg';
+import './assets/img/4geeks.ico';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log('Hello Rigo from the console!');
 };
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = [
+  'before the class',
+  'when I was sleeping',
+  'while I was exercising',
+  'during my lunch',
+  'while I was praying',
+];
+
+function GenerateExcuse() {
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+}
